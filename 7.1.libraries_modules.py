1e63b00535d1cec67bd7ee7fb288c85ebf4b7ff7
ayushchaubeyds01 <ayushchaubeyds@gmail.com>
2026-05-31 14:44:32 +0530

    learnt libraries

---
+++ 7.1.libraries_modules.py
@@ -0,0 +1,44 @@
+#random.choices
+# from random import choice
+# coin=choice(['head','tails'])
+# print(coin)
+
+
+
+# #random.randint(a,b)
+# from random import randint
+# number=randint(1,10)
+# print(number)
+
+
+
+# #random.shuffle(x)
+# from random import shuffle
+# cards=["jack","queen","kings"]
+# shuffle(cards)
+# for card in cards:
+#     print(card)
+
+
+
+# #statistics
+# import statistics
+# print(statistics.mean([100,95,245]))
+
+
+
+
+
+
+
+
+#################### sys
+import sys
+# print("hello my name is",sys.argv[1])
+
+# if len(sys.argv)<2:
+#     print("too few arguements")
+# elif(len(sys.argv))>2:
+#     print("too many arguements")
+# else:
+#     print("my name is ",sys.argv[1])
--- comments.py
+++
@@ -1,4 +0,0 @@
-#ask user for their name
-name=input("enter name:")
-#say hello to user 
-print("hello",name)
