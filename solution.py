5cb8b1c08f4ade10ca4e159517291159520d241e
ayushchaubeyds01 <ayushchaubeyds@gmail.com>
2026-06-05 23:15:12 +0530

    practice

--- solution.py
+++ solution.py
@@ -2344,10 +2344,78 @@
       * *
         *
 '''
-n=int(input("enter n:"))
+# n=int(input("enter n:"))
+# for i in range(n,0,-1):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for k in range(i):
+#         print("*",end="")
+#     print()
+
+
+'''  pyramid
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+'''
+# n=int(input("enter n:"))
+# for i in range(1,n+1):
+#   for j in range(n-i):
+#     print("  ",end="")
+#   for k in range(2*i-1):
+#     print("* ",end="")
+#   print()
+
+
+
+''' reverse pyramid
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
+'''
+# n=int(input("enter n:"))
+# for i in range(n,0,-1):
+#   for j in range(n-i):
+#     print("  ",end="")
+#   for k in range(2*i-1):
+#     print("* ",end="")
+#   print()
+
+
+########practice
+# n=int(input(("enter n:")))
+# for i in range(1,n+1):
+#   print("*"*i)
+  
+# n=int(input(("enter n:")))
+# for i in range(n,0,-1):
+#   print("*"*i)
+
+
+# n=int(input(("enter n:")))
+# for i in range(1,n+1):
+#   for j in range(1,n+1):
+#     print("*",end=)
+#   print()
+
+
+# n=int(input(("enter n:")))
+# for i in range(1,n+1):
+#   for j in range(n-i):
+#     print(" ",end="")
+#   for k in range(i):
+#     print("*",end="")
+#   print()
+
+
+n=int(input(("enter n:")))
 for i in range(n,0,-1):
-    for j in range(n-i):
-        print(" ",end="")
-    for k in range(i):
-        print("*",end="")
-    print()
+  for j in range(n-i):
+    print(" ",end="")
+  for k in range(i):
+    print("*",end="")
+  print()
