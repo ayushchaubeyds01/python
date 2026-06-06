7ccaa99ae862ee40110358649692416791d4a6ea
ayushchaubeyds01 <ayushchaubeyds@gmail.com>
2026-06-06 12:21:21 +0530

    practice

--- solution.py
+++ solution.py
@@ -2448,3 +2448,37 @@
 #   print()
 
 
+''' diamond
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+'''
+# n=int(input("enter n:"))
+# for i in range(1,n+1):
+#   for j in range(n-i):
+#     print("  ",end="")
+#   for _ in range(2*i-1):
+#     print("* ",end="")
+#   print()
+# for i in range(n-1,0,-1):
+#   for j in range(n-i):
+#     print("  ",end="")
+#   for _ in range(2*i-1):
+#     print("* ",end="")
+#   print()
+   
+   
+   
+''' hollow square
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+'''
