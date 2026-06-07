344155b18450f7eed639bc7333cf72ae6c2e0fb0
ayushchaubeyds01 <ayushchaubeyds@gmail.com>
2026-06-07 14:23:46 +0530

    practice

--- solution.py
+++ solution.py
@@ -2482,3 +2482,34 @@
 *       *
 * * * * *
 '''
+# n=int(input("enter n:"))
+# for i in range(1,n+1):
+#   for j in range(1,n+1):
+#     if i==1 or i==n or j==1 or j==n:
+#       print("* ",end="")
+#     else:
+#       print("  ",end="")
+#   print()
+
+
+
+
+
+''' hollow left half pyramid
+* 
+* * 
+*   * 
+*     * 
+* * * * * 
+'''
+n = int(input("Enter n: "))
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+
+        if j == 1 or j == i or i == n:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,8 @@
-for i in range(1,6):
-    print("*"*(6-i))
+n=int(input(("enter n:")))
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    if j==1 or j==i or i==n:
+      print("*",end="")
+    else:
+      print(" ",end="")
+    print()
