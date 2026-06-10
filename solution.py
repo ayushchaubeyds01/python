e3234f386148018ae699e82faa58c04ba116b975
ayushchaubeyds01 <ayushchaubeyds@gmail.com>
2026-06-10 13:19:40 +0530

    practice

--- solution.py
+++ solution.py
@@ -3385,4 +3385,24 @@
 
 
 
-n=int(input("enter n:"))
+
+# n=int(input("enter n:"))
+# for i in range (n,0,-1):
+#   for j in range(n-i):
+#     print(" ",end="")
+#   for k in range(i):
+#     if i==n  or k==0 or k==i-1:
+#       print("* ",end="")
+#     else:
+#       print("  ",end="")
+#   print()
+  
+# for i in range(2,n+1):
+#   for j in range(n-i):
+#     print(" ",end="")
+#   for k in range(i):
+#     if k==0 or i==n or k==i-1:
+#       print("* ",end="")
+#     else:
+#       print("  ",end="")
+#   print()
